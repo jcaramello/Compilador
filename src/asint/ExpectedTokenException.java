6ffdec4a8a89cecf2a31ac69758029136024de7d
Nestor Echegoyen <nechegoyen90529@gmail.com>
2013-09-04 21:02:49 -0300

    expectedtokenexception

---
+++ src/asint/ExpectedTokenException.java
@@ -0,0 +1,9 @@
+package asint;
+
+public class ExpectedTokenException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public ExpectedTokenException(String msg) {
+		super(msg);
+	}
+}
