7c831dc19b3396d2316631140de79ab083b50cb4
Jose Caramello <josecaramello@gmail.com>
2013-11-28 08:27:22 -0300

    fix

--- src/compiler/test/CompilerTest.java
+++ src/compiler/test/CompilerTest.java
@@ -32,7 +32,7 @@ public class CompilerTest {
 	private String expected; // Mensaje de excepción esperado, o null si se espera que tenga éxito
 	
 	public CompilerTest(String testFile, String exp) {
-		Application.Name = testFile;
+		Application.OutputFile = testFile;
 		this.testFile =  testFile;
 		this.expected = exp;
 	}
@@ -156,7 +156,7 @@ public class CompilerTest {
 			   {"ForIncrementoNoConforma.incorrecto", "El tipo de la expresión de incremento en el for debe conformar con el de la variable de iteración."},
 			   {"AsignacionClaseATipoPrimitivo.incorrecto", "Error(!). El tipo del lado derecho de una asignación debe conformar al tipo del lado izquierdo, en línea 8."},
 			   {"AsignacionPrimitivoATipoClase.incorrecto", "Error(!). El tipo del lado derecho de una asignación debe conformar al tipo del lado izquierdo, en línea 7."},
-			   {"LlamadaAMetodoNoVoidConRetornoVacio.incorrecto", "El tipo de la expresión de retorno de un método no-void debe existir, en línea 16"},
+			   {"LlamadaAMetodoNoVoidConRetornoVacio.incorrecto", "El tipo de la expresión de retorno de un método no-void debe existir, en línea 15"},
 			   
 	   });
 	}
